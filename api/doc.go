6e54a140f1d8ff45d6a9fa32426ce113de3472fa
roylee0704 <roylee0704@gmail.com>
2016-08-19 13:04:06 +0800

    add comments

--- api/doc.go
+++ api/doc.go
@@ -1,5 +1,12 @@
 // Package api defines an abstraction for RPCs.
 //
-// APIs are a *fundamental building block* for many Go ion components.
-// APIs are implemented by servers, and called by clients.
+// Conceptually, an API can be generalised as function(input) output. In
+// particular,
+//
+// func api(input) output {
+//   return any_function_at_all(input)
+// }
+//
+// APIs are a *fundamental building block* for many Go ion components. APIs are
+// implemented by servers, and called by clients.
 package api

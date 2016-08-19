b67a22c98964d5edc172ae11360bb5dc41c76f1e
roylee0704 <roylee0704@gmail.com>
2016-08-19 13:30:26 +0800

    be a nice guy

--- api/api.go
+++ api/api.go
@@ -5,7 +5,7 @@ import "context"
 // Api is a fundamental building block of servers and clients.
 // It represents a single RPC method. It is a callback function signature.
 //
-// Or else, think of it as a schematic/diagram to build a fucking gun. In order,
+// Or else, think of it as a schematic/diagram to build a f***ing gun. In order,
 // to build a gun, you need to implement the details.
 //
 // In functional-programming term, we call superCharge. You supercharge with http-details,

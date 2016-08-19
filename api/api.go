92ff6ef5881c90d71c4cd2a3212e406486d682f7
roylee0704 <roylee0704@gmail.com>
2016-08-19 13:44:27 +0800

    add further remarks

--- api/api.go
+++ api/api.go
@@ -13,4 +13,11 @@ import "context"
 //
 // For OOP pals, you may consider this as a mini-version of interface, rule's
 // the same: implement first before actual usage.
+//
+// Sample details?
+// - where: target to fire.
+// - who: people that is gonna hold the gun and fire.
+// - input: what type of bullet. - petrol?
+// - process: how to fire? how to process the input?
+// - output: how does the output looks like. - flame?
 type Api func(ctx context.Context, request interface{}) (response interface{}, err error)
